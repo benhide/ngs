05665745cab419ae65ed8127b3a10bbb1ff06940
benhide <ben.m.hide@googlemail.com>
2019-08-21 14:42:40 +0100

    final commit

--- 6-os/4-bug.js
+++ 6-os/4-bug.js
@@ -6,11 +6,11 @@ function convertArrayToObject(arr) {
 }
 
 const obj = convertArrayToObject([
-  [1, 'One'],
-  [2, 'Two'],
-  [3, 'Three'],
-  [4, 'Four'],
-  [5, 'Five'],
+  [1, "One"],
+  [2, "Two"],
+  [3, "Three"],
+  [4, "Four"],
+  [5, "Five"]
 ]);
 
 console.log(obj);
