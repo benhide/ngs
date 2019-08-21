ad48c6606f2dc4d94d68754fe0fdd665b7cd0b95
benhide <ben.m.hide@googlemail.com>
2019-08-21 14:18:44 +0100

    End of module 7 commit

--- 5-web/2-express/index.js
+++ 5-web/2-express/index.js
@@ -1,7 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
+server.get("/", (reg, res) => {
+  res.send("Hello Express");
+});
+
+server.get("/about", (reg, res) => {
+  res.send("Hello Express - in about");
+});
+
 server.listen(4242, () => {
-  console.log('Express Server is running...');
+  console.log("Express Server is running...");
 });
